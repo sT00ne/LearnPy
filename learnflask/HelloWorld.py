12cc03fd124873b2872414c2aed85bf58fa89da8
st <123>
2016-07-19 16:54:45 +0800

    use flask_scipt,bootstrap,moment

--- learnflask/HelloWorld.py
+++ learnflask/HelloWorld.py
@@ -1,8 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
+
+
+@app.route('/abc')
+def abc():
+    return '<h1>Bad Request</h1>', 400
+
+
+@app.route('/user/<name>')
+def user(name):
+    users = ('a', 'b')
+    return render_template('user.html', name=name, comments=users)
+
+
+@app.route('/buser/<name>')
+def buser(name):
+    return render_template('user_bootstrap.html', name=name, current_time=datetime.utcnow())
+
+
+@app.route('/a')
+def a():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    manager.run()
