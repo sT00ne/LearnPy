50461aa7fa8f3d9770db4cda5a8e3f72159ca399
st <123>
2016-07-21 15:03:48 +0800

    send mail

--- learnflask/HelloWorld.py
+++ learnflask/HelloWorld.py
@@ -1,32 +1,47 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_script import Manager
+from datetime import datetime
+from flask_script import Manager, Shell, Server
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField, validators
 from flask_sqlalchemy import SQLAlchemy
-import os
+from flask_mail import Mail, Message
+from threading import Thread
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+# 数据库配置
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# 邮件发送配置
+app.config['MAIL_SERVER'] = 'smtp.163.com'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = os.environ.get('MAIL_USERNAME')
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app.config['SECRET_KEY'] = 'secretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+mail = Mail(app)
+manager.add_command("runserver", Server(use_debugger=True))
 
 
+# form表单
 class NameForm(Form):
     name = StringField('What is your name?', validators=[validators.data_required()])
     submit = SubmitField('Submit')
 
 
+# 数据库表Role
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -37,6 +52,7 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
+# 数据库表User
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -45,6 +61,31 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+# shell添加数据库
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+
+# 发送邮件
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
+
+# 异步发送邮件
+def send_async_email(mailapp, msg):
+    with mailapp.app_context():
+        mail.send(msg)
 
 
 @app.route('/')
@@ -77,16 +118,21 @@ def a():
 def form():
     form = NameForm()
     if form.validate_on_submit():
+        # 提交
+        # 获取输入的用户名
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
-            session['name'] = form.name.data
-            form.name.data = ''
-            return redirect(url_for('form'))
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('form'))
     return render_template('form.html',
                            form=form, name=session.get('name'),
                            known=session.get('known', False))
